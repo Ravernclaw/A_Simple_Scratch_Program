2451644a1a1d4eecb6dce6d82e126ba11193b369
Ravernclaw <90256414+Ravernclaw@users.noreply.github.com>
2023-03-13 23:52:01 +0800

    Update RMRB.py

--- RMRB.py
+++ RMRB.py
@@ -1,4 +1,4 @@
-﻿# !/usr/bin/env python
+# !/usr/bin/env python
 # -*-coding:utf-8-*-
 #-*-coding:GBK -*-
 import urllib.request
@@ -6,15 +6,13 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-search = str(input('请输入查询内容:'))#关键词
-titles = []
+search = str(input('Please input the keywords:'))#关键词
+titles = []#keywords you wanna search for
 time = []
 m = 0
-for i in range(1, 103):
-	if i == 55 or i == 74: #第55页和74页乱码因此跳过
-		continue
+for i in range(1, 103): #The pages you wanna search
 	url = "http://data.people.com.cn/rmrb/s?qs=" + '''{"cds":[{"cdr":"AND","cds":[{"fld":"title","cdr":"OR","hlt":"true","vlr":"OR","val":"''' +search+ '''"},{"fld":"subTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"''' +search+ '''"},{"fld":"introTitle","cdr":"OR","hlt":"true","vlr":"OR","val":"''' +search+ '''"}]}],"obs":[{"fld":"dataTime","drt":"DESC"}]}&tr=A&ss=1&pageNo='''+str(i)+'''&pageSize=20'''
-	print("搜索的url:",url)
+	print("url:",url)
 	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
 	r = requests.get(url, timeout=30, headers=headers)
 	r.raise_for_status()
@@ -30,7 +28,7 @@ for i in range(1, 103):
 		j = j+1
 		mark = row.find_all('a')[0]
 		#print(mark)
-		print("保存第", m, "条新闻:",mark.get_text()) #标题的数据
+		print("Save", m, "piece news:",mark.get_text()) 
 		record = [mark.get_text(),]
 		titles.append(mark.get_text())
 		columns = row.find_all('div')
@@ -50,4 +48,4 @@ if __name__=="__main__":
 	df = pd.concat([df1, df2], axis=1)
 	print(df)
 	df.to_csv('./data/titles.csv', encoding='utf_8_sig')
-	print('搜素完成，保存目录./new/')
+	print('finished')
